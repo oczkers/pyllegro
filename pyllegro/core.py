7b3e258c9c6ffa2ca71ce1148354a504e0fefeaa
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-08-01 18:07:18 +0200

    use zeep instead of pysimplesoap && fex impovements

--- pyllegro/core.py
+++ pyllegro/core.py
@@ -11,30 +11,35 @@ A lightweight wrapper around the Allegro webapi.
 import sys
 import time
 import logging
-from pysimplesoap.client import SoapClient, SoapFault
-from socket import error as socketError
+from zeep import Client
+from zeep.exceptions import Fault
+from requests.exceptions import ConnectionError
+# from socket import error as socketError
 from decimal import Decimal
 from base64 import b64encode
 from hashlib import sha256
 
-# logger - pysiemplesoap
-logger_pysimplesoap = logging.getLogger('pysimplesoap')
-logger_pysimplesoap.setLevel(logging.ERROR)
+if sys.version_info[0] == 3:
+    long = int
+
+# logger - zeep
+logger_zeep = logging.getLogger('zeep')
+logger_zeep.setLevel(logging.ERROR)
 
 # logger - allegro
-logger_allegro = logging.getLogger('pyllegro')
+logger_allegro = logging.getLogger(__name__)
 
 
 def magicDecode(var):
-    """Decodes unicode to string."""
+    """Decode unicode to string."""
     if var:
         var = var.encode('utf8')
     return var
 
 
 def chunked(l, n):
-    """Chunks one big list into few small lists."""
-    return [l[i:i+n] for i in range(0, len(l), n)]
+    """Chunk one big list into few small lists."""
+    return [l[i:i + n] for i in range(0, len(l), n)]
 
 
 class Allegro(object):
@@ -42,10 +47,12 @@ class Allegro(object):
         self.debug = debug
         self.webapi_url = 'https://webapi.allegro.pl/service.php?wsdl'
         self.username = username
-        self.passwd_hash = b64encode(sha256(passwd).digest())  # hash password
+        self.passwd_hash = b64encode(sha256(passwd.encode('utf-8')).digest()).decode('utf-8')  # hash password
         self.webapi_key = webapi_key
+        self.last_event_id = 0
         # init soap client & login
-        self.client = SoapClient(wsdl=self.webapi_url)
+        # self.client = Client(self.webapi_url)
+        # self.ArrayOfLong = self.client.get_type('ns0:ArrayOfLong')  # this should be done by zeep...
         self.token = self.login(self.username, self.passwd_hash, self.webapi_key)
 
     def __relogin__(self):
@@ -53,20 +60,21 @@ class Allegro(object):
         while True:
             try:
                 return self.login(self.username, self.passwd_hash, self.webapi_key)
-            except socketError as e:
-                logger_allegro.warning(e)
-                time.sleep(5)
-            except SoapFault as e:
-                logger_allegro.warning(e)
-                time.sleep(5)
+            # except socketError as e:
+            #     logger_allegro.warning(e)
+            #     time.sleep(5)
             except:
+                print(sys.exc_info())
+                print('Unknown login error')
                 logger_allegro.warning('Unknown login error')
                 logger_allegro.exception(sys.exc_info())
                 time.sleep(5)
 
     def __ask__(self, service, **kwargs):
-        """Asks allegro (catch errors). Returns response."""
-        if self.debug: print 'ALLEGRO:', service, kwargs  # DEBUG
+        """Ask allegro (catch errors). Returns response."""
+        # TODO: send error code/message to mail
+        if self.debug:
+            print('ALLEGRO: %s %s' % (service, kwargs))  # DEBUG
 
         while True:
             if service not in ('doGetSiteJournalDeals', 'doGetSiteJournalDealsInfo'):
@@ -75,39 +83,61 @@ class Allegro(object):
                 kwargs['sessionId'] = self.token
             # process only if token avaible
             try:
-                return self.client.wsdl_call(service, **kwargs)
-            except socketError as e:
-                logger_allegro.warning(e)
-                time.sleep(5)
-            except SoapFault as e:
-                if e[0] == 'ERR_SESSION_EXPIRED' or e[0] == 'ERR_NO_SESSION':
-                    # logger_allegro.debug('Session expired - relogging.')
-                    logger_allegro.debug(e)
-                    self.token = self.__relogin__()
-                elif e[0] == 'ERR_INTERNAL_SYSTEM_ERROR':
-                    logger_allegro.debug(e)
+                return self.client.service[service](**kwargs)
+            except Fault as e:
+                if e.code in ('ERR_NO_SESSION', 'ERR_SESSION_EXPIRED'):
+                    print('zly identyfikator, relogowanie')
                     time.sleep(5)
-                # elif e[0] == 'ERR_AUCTION_KILLED': # deleted by allegro admin
-                #    pass
+                    self.token = self.__relogin__()
+                elif e.code == 'ERR_INTERNAL_SYSTEM_ERROR':
+                    print('internal server error')
+                    time.sleep(5)
                 else:
-                    logger_allegro.warning(e)
+                    print(sys.exc_info())
+                    print(e)
+                    print(e.code)
                     time.sleep(5)
                     self.token = self.__relogin__()
+            except ConnectionError as e:
+                print('connection error')
+                print(e)
+                time.sleep(5)
+            # except socketError as e:
+            #     logger_allegro.warning(e)
+            #     time.sleep(5)
+            # except SoapFault as e:
+            #     if e[0] == 'ERR_SESSION_EXPIRED' or e[0] == 'ERR_NO_SESSION':
+            #         # logger_allegro.debug('Session expired - relogging.')
+            #         logger_allegro.debug(e)
+            #         self.token = self.__relogin__()
+            #     elif e[0] == 'ERR_INTERNAL_SYSTEM_ERROR':
+            #         logger_allegro.debug(e)
+            #         time.sleep(5)
+            #     # elif e[0] == 'ERR_AUCTION_KILLED': # deleted by allegro admin
+            #     #    pass
+            #     else:
+            #         logger_allegro.warning(e)
+            #         time.sleep(5)
+            #         self.token = self.__relogin__()
             except:
+                print(sys.exc_info())
+                print('Unknown soap error')
                 logger_allegro.warning('Unknown soap error')
                 logger_allegro.exception(sys.exc_info())
                 time.sleep(5)
                 self.token = self.__relogin__()
 
     def login(self, username, passwd_hash, webapi_key, country_code=1):
-        """Logins (sets self.token). Returns token (session_handle)."""
-        ver_key = self.client.doQuerySysStatus(1, 1, webapi_key)['verKey']
-        return self.client.doLoginEnc(username, passwd_hash,
-                                      country_code, webapi_key,
-                                      ver_key)['sessionHandlePart']
+        """Log in (sets self.token). Returns token (session_handle)."""
+        self.client = Client(self.webapi_url)
+        self.ArrayOfLong = self.client.get_type('ns0:ArrayOfLong')  # this should be done by zeep...
+        ver_key = self.client.service.doQuerySysStatus(1, 1, webapi_key)['verKey']
+        return self.client.service.doLoginEnc(username, passwd_hash,
+                                              country_code, webapi_key,
+                                              ver_key)['sessionHandlePart']
 
     def getAuctionDetails(self, auction_id):
-        """Returns basic auction details (doShowItemInfoExt)."""
+        """Return basic auction details (doShowItemInfoExt)."""
         return self.__ask__('doShowItemInfoExt',
                             itemId=auction_id,
                             # getDesc=0,
@@ -118,33 +148,55 @@ class Allegro(object):
                             )  # ['itemListInfoExt']
 
     def getBids(self, auction_id):
-        """Returns bids."""
+        """Retrieve all bids in given auction."""
         bids = {}
-        rc = self.__ask__('doGetBidItem2', itemId=auction_id)['biditemList']
-        for i in rc:
-            i = i['item']['bidsArray']
-            bids[long(i[1]['item'])] = {
-                'price': Decimal(i[6]['item']),
-                'quantity': int(i[5]['item']),
-                'date_buy': i[7]['item']
-            }
+        rc = self.__ask__('doGetBidItem2', itemId=auction_id)
+        if rc:
+            for i in rc:
+                i = i['bidsArray']
+                bids[long(i['item'][1])] = {
+                    'price': Decimal(i['item'][6]),
+                    'quantity': int(i['item'][5]),
+                    'date_buy': i['item'][7]
+                }
         return bids
 
+    def getBuyerInfo(self, auction_id, buyer_id):
+        """Return buyer info."""
+        # TODO: add price from getBids
+        rc = self.__ask__('doGetPostBuyData', itemsArray=self.ArrayOfLong([auction_id]), buyerFilterArray=self.ArrayOfLong([buyer_id]))
+        rc = rc[0]['usersPostBuyData']['item'][0]['userData']
+        return {'allegro_aid': auction_id,
+                'allegro_uid': rc['userId'],
+                'allegro_login': magicDecode(rc['userLogin']),
+                'name': magicDecode(rc['userFirstName']),
+                'surname': magicDecode(rc['userLastName']),
+                'company': magicDecode(rc['userCompany']),
+                'postcode': magicDecode(rc['userPostcode']),
+                'city': magicDecode(rc['userCity']),
+                'address': magicDecode(rc['userAddress']),
+                'email': magicDecode(rc['userEmail']),
+                'phone': rc['userPhone']}
+
     def getOrders(self, auction_ids):
-        """Returns orders details."""
+        """Return orders details."""
         orders = {}
         # chunk list (only 25 auction_ids per request)
         for chunk in chunked(auction_ids, 25):
-            auctions = [{'item': auction_id} for auction_id in chunk]
-            rc = self.__ask__('doGetPostBuyData', itemsArray=auctions)['itemsPostBuyData']  # [0]['item']
+            # auctions = [{'item': auction_id} for auction_id in chunk]  # TODO?: is it needed?
+            auctions = self.ArrayOfLong(chunk)
+            rc = self.__ask__('doGetPostBuyData', itemsArray=auctions)
             for auction in rc:
-                auction = auction['item']
                 orders_auction = []
                 bids = self.getBids(auction['itemId'])
                 # get orders details
-                for i in auction.get('usersPostBuyData', ()):
-                    i = i['item']['userData']
-                    if i['userId'] not in bids: continue  # temporary(?) webapi bug fix
+                # for i in auction.get('usersPostBuyData', ()):
+                if not auction['usersPostBuyData']:  # empty
+                    continue
+                for i in auction['usersPostBuyData']['item']:
+                    i = i['userData']
+                    if i['userId'] not in bids:  # temporary(?) webapi bug fix
+                        continue
                     orders_auction.append({
                         'allegro_aid': auction['itemId'],
                         'allegro_uid': i['userId'],
@@ -165,7 +217,7 @@ class Allegro(object):
         return orders
 
     def getTotalPaid(self, auction_id, buyer_id):
-        """Returns total paid from buyer on single auction."""
+        """Return total paid from buyer on single auction."""
         # TODO: it has to be better way to check payments.
         date_end = long(time.time())
         date_start = date_end - 60 * 60 * 24 * 90
@@ -175,53 +227,60 @@ class Allegro(object):
                           transRecvDateFrom=date_start,
                           transRecvDateTo=date_end,
                           transPageLimit=25,  # notneeded | TODO: can be more than 25 payments
-                          transOffset=0)['payTransIncome']
+                          transOffset=0)
         paid = 0
-        for t in rc:
-            t = t['item']
+        for t in (rc or []):
+            # t = t['item']
             if t['payTransStatus'] == u'Zakończona' and t['payTransIncomplete'] == 0:
                 if t['payTransItId'] == 0:  # wplata laczna
-                    for td in t['payTransDetails']:
-                        td = td['item']
+                    for td in t['payTransDetails']['item']:
                         if td['payTransDetailsItId'] == auction_id:
                             paid += Decimal(str(td['payTransDetailsPrice']))
                 else:  # wplata pojedyncza
                     paid += Decimal(str(t['payTransAmount']))
         return paid
 
+    def getJournal(self, start=0):
+        """Get all journal events from start."""
+        # TODO: while len(journaldeals) < 100
+        pass
+
     def getJournalDealsInfo(self, start=0):
-        """Returns all deals ammount (from start)."""
+        """Return all events ammount (from start)."""
         rc = self.__ask__('doGetSiteJournalDealsInfo',
-                          journalStart=start)['siteJournalDealsInfo']
+                          journalStart=start)
         return rc['dealEventsCount']
 
-    def getJournalDeals(self, start=0):
-        """Returns all journal deals from start."""
-        deals = []
-        while self.getJournalDealsInfo(start) > 0:
-            rc = self.__ask__('doGetSiteJournalDeals',
-                              journalStart=start)['siteJournalDeals']
+    def getJournalDeals(self, start=None):
+        """Return all journal events from start."""
+        # 1 - utworzenie aktu zakupowego (deala), 2 - utworzenie formularza pozakupowego (karta platnosci), 3 - anulowanie formularza pozakupowego (karta platnosci), 4 - zakończenie (opłacenie) transakcji przez PzA
+        if start is not None:
+            self.last_event_id = start
+        events = []
+        while self.getJournalDealsInfo(self.last_event_id) > 0:
+            rc = self.__ask__('doGetSiteJournalDeals', journalStart=self.last_event_id)
             for i in rc:
-                deals.append({
-                    'deal_id': i['item']['dealId'],
-                    'deal_status': i['item']['dealEventType'],
-                    'transaction_id': i['item']['dealTransactionId'],
-                    'time': i['item']['dealEventTime'],
-                    'event_id': i['item']['dealEventId'],
-                    'auction_id': i['item']['dealItemId'],
-                    'buyer_id': i['item']['dealBuyerId'],
-                    'quantity': i['item']['dealQuantity']
+                events.append({
+                    'allegro_did': i['dealId'],
+                    'deal_status': i['dealEventType'],
+                    'transaction_id': i['dealTransactionId'],
+                    'time': i['dealEventTime'],
+                    'event_id': i['dealEventId'],
+                    'allegro_aid': i['dealItemId'],
+                    'allegro_uid': i['dealBuyerId'],
+                    # 'seller_id': i['dealSellerId '],
+                    'quantity': i['dealQuantity']
                 })
-            start = rc[-1]['item']['dealEventId']
-        return deals
+            self.last_event_id = rc[-1]['dealEventId']
+        return events
 
     # feedback
     def getWaitingFeedbacks(self):
-        """Returns all waiting feedbacks from buyers."""
+        """Return all waiting feedbacks from buyers."""
         # TODO: return sorted dictionary (negative/positive/neutral)
         feedbacks = []
         offset = 0
-        amount = self.__ask__('doGetWaitingFeedbacksCount')['feedbackCount']
+        amount = self.__ask__('doGetWaitingFeedbacksCount')
         while amount > 0:
             rc = self.__ask__('doGetWaitingFeedbacks',
                               offset=offset, packageSize=200)
--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         # 'Programming Language :: Python :: Implementation :: CPython',  # not tested
         # 'Programming Language :: Python :: Implementation :: IronPython',  # not tested
         'Programming Language :: Python :: Implementation :: PyPy',
